dd3faab91c241e2136b065fcf8669f62169b46bf
lynnsamuelson <lynn.samuelson@gmail.com>
2015-11-19 12:07:09 -0600

    notes from Nov19

--- Jitter/Models/Jot.cs
+++ Jitter/Models/Jot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,11 @@ namespace Jitter.Models
 {
     public class Jot
     {
+
         public object Author { get; set; }
         public string Content { get; set; }
         public DateTime Date { get; set; }
+        [Key]
         public int JotId { get; set; }
         public string Picture { get; set; }
     }
